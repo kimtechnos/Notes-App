a99c74c627a4f75facdd50e048f98d571bf1743b
kimtechnos <franciskimani559@gmail.com>
2024-10-29 18:58:00 +0300

    implemented the read funtion for the notes app

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import fs from "fs";
+import fs, { read } from "fs";
 import { Command } from "commander";
+import chalk from "chalk";
 const program = new Command();
 
 program
@@ -37,22 +38,35 @@ program
     notes = JSON.parse(loadedNotes);
     const noteExists = notes.find((currentNote) => currentNote.title === title);
     if (noteExists) {
-      console.log(`Note with  title '${title}' already exists.`);
+      console.log(chalk.bgRed(`Note with  title '${title}' already exists.`));
       return;
     }
 
     notes.push(newNote);
     fs.writeFileSync("./data/notes.json", JSON.stringify(notes));
-
-    // Adding the new note to the notes array
-    // notesArray.push(newNote);
-    // fs.writeFileSync("./data/notes.json", JSON.stringify(notesArray));
-
-    // console.log(notesArray);
+    console.log(chalk.bgGreen(`NEW NOTE ADDED Successfully!!`));
+  });
+//read the notes we currently have
+program
+  .command("read")
+  .description("Display all the notes")
+  .action(function () {
+    const loadedNotes = fs.readFileSync("./data/notes.json", "utf8");
+    const notes = JSON.parse(loadedNotes);
+    if (notes.length === 0) {
+      console.log(chalk.bgYellow("you do not have any notes yet"));
+      return;
+    }
+    notes.forEach((currentNote) => {
+      console.log(chalk.bgBlue("======"));
+      console.log(currentNote.title);
+      console.log("------");
+      console.log(currentNote.body);
+      console.log(chalk.bgBlue("======\n"));
+    });
   });
 
 program.parse(process.argv);
-//read the notes we currently have
 
 //push the new notes to the array returned
 // write the updated arrya to the file
