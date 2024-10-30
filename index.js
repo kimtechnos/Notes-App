931cfc80d5e413d0b4bad2b8bffb61d6c125e967
kimtechnos <franciskimani559@gmail.com>
2024-10-30 17:45:14 +0300

    add the delete notes function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import fs, { read } from "fs";
 import { Command } from "commander";
 import chalk from "chalk";
+import { title } from "process";
 const program = new Command();
 
 program
@@ -49,21 +50,65 @@ program
 //read the notes we currently have
 program
   .command("read")
-  .description("Display all the notes")
+  .description("Display all the notes or specific note when we pass t flag ")
+  .option("-t , title <value>", "title of the note to be read")
   .action(function () {
     const loadedNotes = fs.readFileSync("./data/notes.json", "utf8");
     const notes = JSON.parse(loadedNotes);
+
     if (notes.length === 0) {
       console.log(chalk.bgYellow("you do not have any notes yet"));
       return;
+    }
+    if (title) {
+      const note = notes.find((currentNote) => currentNote.title === title);
+      if (note) {
+        console.log(note.title);
+        console.log(note.body);
+        return;
+      }
+      console.log(chalk.bgRed("No note with title ${title}"));
     }
     notes.forEach((currentNote) => {
       console.log(chalk.bgBlue("======"));
       console.log(currentNote.title);
       console.log("------");
       console.log(currentNote.body);
+      console.log(
+        `created at: ${new Date(currentNote.createdAt).toISOString()}`,
+      );
       console.log(chalk.bgBlue("======\n"));
     });
+  });
+//delete notes
+program
+  .command("delete")
+  .description("deletes a specific note")
+  .option("-t , --title <value>", "title of the note to be deleted")
+  .action(function (options) {
+    const title = options.title;
+    const loadedNotes = fs.readFileSync("./data/notes.json", "utf8");
+    const notes = JSON.parse(loadedNotes);
+    if (notes.length === 0) {
+      console.log(chalk.bgYellow("Nothing to delete"));
+      return;
+    }
+    const remainingNotes = notes.filter(
+      (currentNote) => currentNote.title !== title,
+    );
+    if (remainingNotes.length === notes.length) {
+      console.log(
+        chalk.bgRed(
+          `could not delete note with title '${title}' .it doesnt exist`,
+        ),
+      );
+      return;
+    }
+
+    fs.writeFileSync("./data/notes.json", JSON.stringify(remainingNotes));
+    console.log(
+      chalk.bgGreen(`note with title '${title}'deleted successfully `),
+    );
   });
 
 program.parse(process.argv);
